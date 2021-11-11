594c8d2ef23c8ccc2de8892b39502006f888feef
Carolina Romanos <carolina.romanos.roca@gmail.com>
2021-11-11 18:49:10 +0100

    done

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: {type: String, unique: true, required: true },
+  level: {type: String, enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]},
+  ingredients: [{type: String }],
+  cuisine: {type: String },
+  dishType: {type: String },
+  image: {type: String },
+  duration: {type: Number },
+  creator: {type: String }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
